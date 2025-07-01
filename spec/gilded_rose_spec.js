c58029f7735324da1b36649e86104ed661fbc839
erionpolisi <er.polisi@gmail.com>
2025-07-01 10:11:32 +0200

    Tests: Vollständige Abdeckung des Legacy-Codes (normal, Brie, Sulfuras, Backstage)

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,5 +1,10 @@
 describe("Gilded Rose", function() {
 
+  //===============================LEGACY CODE==============================
+
+
+  // === Normal Items ===
+
   it("should decrease SellIn and Quality by 1 for normal items", function() {
   items = [ new Item("Elixir of the Mongoose", 5, 7) ];
   update_quality();
@@ -19,6 +24,8 @@ it("should not decrease Quality below 0", function() {
   update_quality();
   expect(items[0].quality).toEqual(0);
 });
+
+// === Aged Brie ===
 
 it("should increase Quality of Aged Brie as it gets older", function() {
   items = [ new Item("Aged Brie", 2, 0) ];
@@ -40,12 +47,16 @@ it("should not increase Quality of Aged Brie beyond 50", function() {
   expect(items[0].quality).toEqual(50);
 });
 
+// === Sulfuras ===
+
 it("should not change SellIn or Quality for Sulfuras", function() {
   items = [ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ];
   update_quality();
   expect(items[0].sell_in).toEqual(0);
   expect(items[0].quality).toEqual(80);
 });
+
+// === Backstage Passes ===
 
 it("should increase Quality by 1 for Backstage passes when SellIn > 10", function() {
   items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) ];
@@ -74,5 +85,7 @@ it("should drop Quality to 0 for Backstage passes after SellIn < 0", function() 
   expect(items[0].sell_in).toEqual(-1);
   expect(items[0].quality).toEqual(0);
 });
+
+//==============================================================
 
 });
